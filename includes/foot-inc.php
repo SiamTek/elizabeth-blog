14ddb1ca6259f26a8970ce2fae350f06ae2cd5c8
Diego <dluceroppc@gmail.com>
2014-04-02 14:27:09 -0600

    Added an include for the sidebar

--- includes/foot-inc.php
+++ includes/foot-inc.php
@@ -1,32 +1,33 @@
-<footer class="text-center">
-	<div class="row">
-		<section class="col-sm-4">
-			<ul>
-				<li><a href="index.html">Home</a></li>
-				<li><a href="about.html">About</a></li>
-				<li><a href="blog.html">Blog</a></li>
-				<li><a href="contact.html">Contact</a></li>
-			</ul>
-		</section>
-		<section class="col-sm-4">
-			<h4>&copy; 2014 Design by Siam Tek.</h4>
-		</section>
-		<section class="col-sm-4">
-			Back to top Icon
-		</section>
-	</div><!-- row close -->
-</footer>
-</div><!-- container close -->
 
-<script src="js/lib/jquery.1.10.2.js"></script>
-<script>
-	window.onload = function() {
-	    if (window.jQuery) {  
-	        // jQuery is loaded  
-	        console.log("jQuery Loaded");
-	    } else {
-	        // jQuery is not loaded
-	        console.log("jQuery NOT Loaded");
-	    }
-	}
-</script>
+	<footer class="text-center">
+		<div class="row">
+			<section class="col-sm-4">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="blog.html">Blog</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</section>
+			<section class="col-sm-4">
+				<h4>&copy; 2014 Design by Siam Tek.</h4>
+			</section>
+			<section class="col-sm-4">
+				Back to top Icon
+			</section>
+		</div><!-- row close -->
+	</footer>
+	</div><!-- container close -->
+
+	<script src="js/lib/jquery.1.10.2.js"></script>
+	<script>
+		window.onload = function() {
+			if (window.jQuery) {  
+				// jQuery is loaded  
+				console.log("jQuery Loaded");
+			} else {
+				// jQuery is not loaded
+				console.log("jQuery NOT Loaded");
+			}
+		}
+	</script>
