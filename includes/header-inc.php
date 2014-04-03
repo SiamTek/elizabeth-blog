7ac07e073b0ed6f730ba90d05aa95aea91ec10fd
Diego <dluceroppc@gmail.com>
2014-04-03 15:24:26 -0600

    Removed the hgroup tag {deprecated}
    see the link here for more info:
    http://www.webmonkey.com/2013/04/w3c-drops-hgroup-tag-from-html5-spec/

--- includes/header-inc.php
+++ includes/header-inc.php
@@ -1,5 +1,5 @@
 	<div class="container">
-		<hgroup class="text-center">
+		<div class="hgroup text-center">
 			<h1>Consistently Inconsistent</h1>
 				<h3>by Elizabeth Joy</h3>
-		</hgroup>
+		</div>
