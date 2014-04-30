0e6d502228ecb73360d3f00fb1bfc054477c3266
Diego <dluceroppc@gmail.com>
2014-04-30 02:16:37 -0600

    initial  rough commit
    Added a basic mobile nav. Not finished with media queries or styling.

---
+++ includes/head.php
@@ -0,0 +1,22 @@
+
+	<!--[if lt IE 9]> <script src="js/lib/html5shiv.js"></script> <![endif]-->
+		<link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Oswald:400' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Lusitana:400,700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/reset.css" media="screen">
+		<link rel="stylesheet" href="css/grid.css" media="screen">
+		<link rel="stylesheet" href="css/style.css" media="screen">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1">
+	    <meta name="keywords" content="">
+	    <meta name="author" content="">
+	    <meta name="copyright" content="" />
+	    <meta name="geo.region" content="" />
+	    <meta name="geo.placename" content="" />
+	    <meta name="geo.position" content="" />
+	    <meta name="ICBM" content="" />
+	    <meta charset="UTF-8">
+
+    
+	
---
+++ includes/header.php
@@ -0,0 +1,26 @@
+	
+<div class="container">
+	<header>
+		<div class="hgroup text-center">
+			<a href="index.php" class="main_title"><h1>Consistently Inconsistent</h1></a>
+		</div>
+		<a href="#" id="logo"></a>
+		<nav class="mobile-nav">
+			<a href="#" id="menu-icon"></a>
+			<ul>
+				<li><a href="#" class="current">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Work</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+			
+		<nav class="fancy text-center">
+			<a href="rants.php"><span>Rants</span><span>&amp; Raves</span></a>
+			<a href="blog.php"><span>Inspiration</span><span>&amp; Zen</span></a>
+			<a href="cooking.php"><span>Healthy Living</span><span>&amp; Cooking</span></a>
+			<a href="cats.php"><span>Thoughts</span><span>&amp; Cats</span></a>
+			<a href="contact.php"><span>About</span><span>&amp; Contact Me </span></a>
+		</nav>
+	</header>
