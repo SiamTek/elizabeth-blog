9bfde275564851a11706c2bc887b03d3dc30852e
Diego <dluceroppc@gmail.com>
2014-05-09 15:43:19 -0600

    Temp WP Them dev dir; basic structure

---
+++ WP/inc/footer.php
@@ -0,0 +1,40 @@
+<footer class="text-center">
+	<div class="row"><!-- ROW BEGIN -->
+
+		<section class="col-sm-4 foot-menu">
+
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="about.html">Inspiration</a></li>
+				<li><a href="blog.html">Healthy Living</a></li>
+				<li><a href="contact.html">Thoughts</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</section>
+
+		<section class="col-sm-4 copyright">
+			<h4>&copy; <?php echo date('Y'); ?> Design by <a href="http://siamtek.com" title="Siam Tek: Technology, Entrepreneurship & Business" target="_blank">Siam Tek</a>.</h4>
+		</section>
+		<section class="col-sm-4 back-to-top">
+			Back to top Icon
+		</section>
+	</div><!-- ROW CLOSE -->
+
+</footer>
+</div><!-- CONTAINER CLOSE -->
+
+	<script src="js/lib/jquery.1.10.2.js"></script>
+	<script>
+		window.onload = function() {
+			if (window.jQuery) {  
+				// jQuery is loaded  
+				console.log("jQuery Loaded");
+			} else {
+				// jQuery is not loaded
+				console.log("jQuery NOT Loaded");
+			}
+		}
+	</script>
+	
+</body>
+</html>
